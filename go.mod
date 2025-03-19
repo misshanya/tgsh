da9eafb97448f0fe8e5a2850f764351738f47c5f
misshanya <mishanyadev@gmail.com>
2025-03-19 18:19:46 +0300

    Add windows support

--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.24
 require (
 	github.com/go-telegram/bot v1.14.1
 	github.com/joho/godotenv v1.5.1
+	golang.org/x/text v0.23.0
 )
--- internal/service/service.go
+++ internal/service/service.go
@@ -4,10 +4,16 @@ import (
 	"context"
 	"log/slog"
 	"os/exec"
+	"runtime"
+
+	"golang.org/x/text/encoding/charmap"
+	"golang.org/x/text/transform"
 )
 
 type Service interface {
 	ExecuteCommand(ctx context.Context, command string) (string, error)
+	executeCommandUnix(ctx context.Context, command string) (string, error)
+	executeCommandWindows(ctx context.Context, command string) (string, error)
 }
 
 type service struct{}
@@ -17,6 +23,13 @@ func NewService() Service {
 }
 
 func (s *service) ExecuteCommand(ctx context.Context, command string) (string, error) {
+	if runtime.GOOS == "windows" {
+		return s.executeCommandWindows(ctx, command)
+	}
+	return s.executeCommandUnix(ctx, command)
+}
+
+func (s *service) executeCommandUnix(ctx context.Context, command string) (string, error) {
 	cmd := exec.CommandContext(ctx, "sh", "-c", command)
 	out, err := cmd.CombinedOutput()
 	if err != nil {
@@ -24,4 +37,18 @@ func (s *service) ExecuteCommand(ctx context.Context, command string) (string, e
 		return "", err
 	}
 	return string(out), nil
+}
+
+func (s *service) executeCommandWindows(ctx context.Context, command string) (string, error) {
+	cmd := exec.CommandContext(ctx, "cmd", "/C", command)
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		slog.Error("Error executing command", slog.String("command", command), slog.Any("error", err), slog.String("output", string(out)))
+		return "", err
+	}
+
+	// From Windows1251 to UTF-8
+	decoder := charmap.Windows1251.NewDecoder()
+	utf8out, _, _ := transform.String(decoder, string(out))
+	return utf8out, nil
 }
